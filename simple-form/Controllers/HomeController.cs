eeb8da113064655bcd37209094a0b5662c6e03d1
betasaturday <evgeniya.knm@gmail.com>
2015-08-02 15:47:03 +0300

    add echo method for reposting form data

---
+++ simple-form/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace simple_form.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return Redirect("/Registration.html");
+        }
+
+        public ActionResult Echo()
+        {
+            string[] keys = Request.Form.AllKeys;
+            
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Response.Write(keys[i] + ": <strong>" + Request.Form[keys[i]] + "</strong><br>");
+            }
+            return View();
+        }
+    }
+}
