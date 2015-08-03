56cb84d379b4fb3553f2591901a5232c43f0d545
betasaturday <evgeniya.knm@gmail.com>
2015-08-03 22:03:13 +0300

    AJaX request for main types of form fields

--- simple-form/js/main.js
+++ simple-form/js/main.js
@@ -1,13 +1,47 @@
-(function(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("get", "hello.txt");
+(function () {
+    var submit = document.querySelector("input[type=submit]");
+    submit.addEventListener("click",
+        function (e) {
+            e.preventDefault();
+            sendAJAXRequest(getQueryString());
+        });
+})();
+
+function getQueryString() {
+    var elements = document.querySelectorAll("input[type=text]"), queryString = "", i;
     
-    xhr.addEventListener("readystatechange", function(){
-        if (xhr.readyState == 4)
-        {
+    for (i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var name = element.name;
+        var value = element.value;
+        
+        queryString = queryString + encodeURIComponent(name) + "=" + encodeURIComponent(value) + "&";
+    }
+    var select = document.querySelector("[name=month]");
+    var value = select.options[select.selectedIndex].value;
+    
+    queryString = queryString + "month=" + encodeURIComponent(value) + "&";
+    
+    var radio = document.querySelector("[name=question]:checked");
+    var value = radio.value;
+    
+    queryString += "question=" + encodeURIComponent(value) + "&";
+    
+    console.log(queryString.slice(-1));
+    return queryString;
+}
+
+function sendAJAXRequest(qs) {
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("post", "/Home/Echo/send", true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    
+    xhr.addEventListener("readystatechange", function() {
+        if (xhr.readyState == 4) {
             console.log(xhr.responseText);
         }
     });
-
-    xhr.send();
-})();
+    
+    xhr.send(qs);
+}
