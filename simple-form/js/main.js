8a6b96c351edb2de38b0720b06aa1171bc297dd2
betasaturday <evgeniya.knm@gmail.com>
2015-08-03 09:11:55 +0300

    simple ajax request for txt

--- simple-form/js/main.js
+++ simple-form/js/main.js
@@ -1,3 +1,13 @@
 (function(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", "hello.txt");
     
+    xhr.addEventListener("readystatechange", function(){
+        if (xhr.readyState == 4)
+        {
+            console.log(xhr.responseText);
+        }
+    });
+
+    xhr.send();
 })();
